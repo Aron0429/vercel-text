81753fccf9cfd90e7d36eb6764e066622d1972cb
HuXiaoDai <416681736@qq.com>
2025-05-03 19:08:43 +0800

    test3

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-const fs = require('fs')
 const cors = require('cors')
 const express = require('express')
 const AV = require('leancloud-storage')
 
 const { LEAN_ID, LEAN_KEY, LEAN_MASTER_KEY, LEAN_SERVER } = process.env
 
-// process.env.
-AV.Cloud.useMasterKey(true)
-AV.init({
-  appId: LEAN_ID,
-  appKey: LEAN_KEY,
-  masterKey: LEAN_MASTER_KEY,
-  serverURL: LEAN_SERVER,
-})
+if (LEAN_ID && LEAN_KEY && LEAN_MASTER_KEY) {
+  AV.Cloud.useMasterKey(true)
+  AV.init({
+    appId: LEAN_ID,
+    appKey: LEAN_KEY,
+    masterKey: LEAN_MASTER_KEY,
+    serverURL: LEAN_SERVER,
+  })
+  const Talks = AV.Object.extend('Talks')
+  const talks = new Talks()
+}
 
 const app = express()
-const Talks = AV.Object.extend('Talks')
-const talks = new Talks()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const path = './static/shuoshuo.json'
-
 // 简单写一个接口
 app.post('/create', async (req, res) => {
   const { author, date, content, tags } = req.body
+  talks.set('author', author)
+  talks.set('date', date)
+  talks.set('content', content)
+  talks.set('tags', tags.split(','))
 
-  const query = new AV.Query('Counter')
+  talks.save().then(
+    talk => {
+      res.send('成功')
+    },
+    error => {
+      res.send(error)
+    },
+  )
+})
+
+// 简写写一个获取用户的接口
+app.get('/getTalk', (req, res) => {
+  const query = new AV.Query('Talks')
   query.find().then(
     counters => {
       res.send(counters)
@@ -37,46 +51,6 @@ app.post('/create', async (req, res) => {
       res.send(error)
     },
   )
-
-  // talks.set('author', author)
-  // talks.set('date', date)
-  // talks.set('content', content)
-  // talks.set('tags', tags.split(','))
-
-  // talks.save().then(
-  //   talk => {
-  //     res.send('成功')
-  //   },
-  //   error => {
-  //     console.log(error)
-  //     res.send('失败')
-  //   },
-  // )
-})
-
-// 简写写一个获取用户的接口
-app.get('/getTalk', (req, res) => {
-  if (fs.existsSync(path)) {
-    fs.readFile(path, (err, data) => {
-      if (err) {
-        console.error('读取文件失败:', err)
-        return
-      }
-      content = JSON.parse(data)
-      content.forEach(item => {
-        if (typeof item.tags === 'string') {
-          item.tags = JSON.parse(item.tags)
-        }
-      })
-      res.status(200).send(content)
-    })
-  } else {
-    res.status(200).json({
-      code: 500,
-      msg: 'error',
-      data: null,
-    })
-  }
 })
 
 app.listen(9000, () => {

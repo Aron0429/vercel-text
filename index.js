9c685754a79bc4f1d794c9402985150fa5971852
HuXiaoDai <416681736@qq.com>
2025-05-03 18:59:45 +0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const fs = require('fs')
 const cors = require('cors')
 const express = require('express')
+const AV = require('leancloud-storage')
+
+const { LEAN_ID, LEAN_KEY, LEAN_MASTER_KEY, LEAN_SERVER } = process.env
+
+// process.env.
+AV.Cloud.useMasterKey(true)
+AV.init({
+  appId: 'lwbXnvoNIqG3qCB4hegleYrS-MdYXbMMI',
+  appKey: 'b5Yx8DMBXQVplVwWcrI5niFF',
+  masterKey: 'xyKAWlk6uyk44m10yoU2gF4g',
+  serverURL: LEAN_SERVER,
+})
+
 const app = express()
+const Talks = AV.Object.extend('Talks')
+const talks = new Talks()
 
 app.use(cors())
 app.use(express.json())
@@ -9,33 +24,34 @@ app.use(express.urlencoded({ extended: false }))
 
 const path = './static/shuoshuo.json'
 
-const writeFile = (path, content, res) => {
-  fs.writeFile(path, content, err => {
-    if (err) {
-      console.error('写入文件失败:', err)
-      return
-    }
-    res.send('成功')
-  })
-}
-
 // 简单写一个接口
-app.post('/create', (req, res) => {
-  let content = JSON.stringify([req.body], null, 2)
-  if (fs.existsSync(path)) {
-    fs.readFile(path, (err, data) => {
-      if (err) {
-        console.error('读取文件失败:', err)
-        return
-      }
-      content = JSON.parse(data)
-      content.push(req.body)
-      writeFile(path, JSON.stringify(content), res)
-      res.send('成功')
-    })
-  } else {
-    writeFile(path, content, res)
-  }
+app.post('/create', async (req, res) => {
+  const { author, date, content, tags } = req.body
+
+  const query = new AV.Query('Counter')
+  query.find().then(
+    counters => {
+      res.send(counters)
+    },
+    error => {
+      res.send(error)
+    },
+  )
+
+  // talks.set('author', author)
+  // talks.set('date', date)
+  // talks.set('content', content)
+  // talks.set('tags', tags.split(','))
+
+  // talks.save().then(
+  //   talk => {
+  //     res.send('成功')
+  //   },
+  //   error => {
+  //     console.log(error)
+  //     res.send('失败')
+  //   },
+  // )
 })
 
 // 简写写一个获取用户的接口

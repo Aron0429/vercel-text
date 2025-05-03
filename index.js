e6d96a5dc166c7080c8ad7c6e13bcf02bc7e7c19
HuXiaoDai <416681736@qq.com>
2025-05-03 23:23:21 +0800

    测试接口

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ const AV = require('leancloud-storage')
 
 const { LEAN_ID, LEAN_KEY, LEAN_MASTER_KEY, LEAN_SERVER } = process.env
 
-if (LEAN_ID && LEAN_KEY && LEAN_MASTER_KEY) {
-  AV.Cloud.useMasterKey(true)
-  AV.init({
-    appId: LEAN_ID,
-    appKey: LEAN_KEY,
-    masterKey: LEAN_MASTER_KEY,
-    serverURL: LEAN_SERVER,
-  })
-  const Talks = AV.Object.extend('Talks')
-  const talks = new Talks()
-}
+AV.Cloud.useMasterKey(true)
+AV.init({
+  appId: LEAN_ID,
+  appKey: LEAN_KEY,
+  masterKey: LEAN_MASTER_KEY,
+  serverURL: LEAN_SERVER,
+})
 
 const app = express()
 
@@ -25,6 +21,9 @@ app.use(express.urlencoded({ extended: false }))
 // 简单写一个接口
 app.post('/create', async (req, res) => {
   const { author, date, content, tags } = req.body
+
+  const Talks = AV.Object.extend('Talks')
+  const talks = new Talks()
   talks.set('author', author)
   talks.set('date', date)
   talks.set('content', content)
@@ -41,16 +40,16 @@ app.post('/create', async (req, res) => {
 })
 
 // 简写写一个获取用户的接口
-app.get('/getTalk', (req, res) => {
-  const query = new AV.Query('Talks')
-  query.find().then(
-    counters => {
-      res.status(200).send(counters)
-    },
-    error => {
-      res.status(200).send(error)
-    },
-  )
+app.get('/getTalk', async (req, res) => {
+  try {
+    const query = new AV.Query('Talks')
+    query.dscending('createdAt')
+
+    const talkList = await query.find()
+    res.status(200).send(talkList)
+  } catch (error) {
+    res.status(500).send({ code: 500, message: error.message, data: JSON.stringify(error) })
+  }
 })
 
 app.listen(9000, () => {
